931dec2e96c7ad03947424ff33b1b7d75c43547b
Kazi Shahidul Islam <mindhillinformatics@gmail.com>
2018-05-22 18:56:10 +0400

    Initial Commit

---
+++ buildSchema/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buildSchema
+//
+//  Created by ALAM on 5/22/18.
+//  Copyright © 2018 Mindhill Informatics. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
