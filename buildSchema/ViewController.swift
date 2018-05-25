0e98f480b0c32eb7bb5ad7d290eb76993bb0a21b
Kazi Shahidul Islam <mindhillinformatics@gmail.com>
2018-05-25 22:47:40 +0400

    Second Test of commit for my another project - buildSchema

--- buildSchema/ViewController.swift
+++ buildSchema/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
+    @IBAction func buttonClicked(_ sender: Any) {
+        print("This is just test")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
